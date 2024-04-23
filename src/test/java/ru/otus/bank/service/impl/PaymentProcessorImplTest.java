7791850197c552b4c1c9cf67d69c2bbfe68fe1f5
AndrewBox <AndrewBoxMail@yandex.ru>
2024-04-23 22:54:22 +0300

    hw-01 добавил тесты на исключения

--- src/test/java/ru/otus/bank/service/impl/PaymentProcessorImplTest.java
+++ src/test/java/ru/otus/bank/service/impl/PaymentProcessorImplTest.java
@@ -9,10 +9,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.bank.entity.Account;
 import ru.otus.bank.entity.Agreement;
 import ru.otus.bank.service.AccountService;
+import ru.otus.bank.service.exception.AccountException;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.argThat;
 import static org.mockito.Mockito.when;
 
@@ -58,6 +60,57 @@ public class PaymentProcessorImplTest {
         paymentProcessor.makeTransfer(sourceAgreement, destinationAgreement,
                 0, 0, BigDecimal.ONE);
 
+    }
+
+    @Test
+    public void makeTransferWithNoSourceAccount() {
+        Agreement sourceAgreement = new Agreement();
+        sourceAgreement.setId(1L);
+
+        Agreement destinationAgreement = new Agreement();
+        destinationAgreement.setId(2L);
+
+        when(accountService.getAccounts(argThat(new ArgumentMatcher<Agreement>() {
+            @Override
+            public boolean matches(Agreement argument) {
+                return argument != null && argument.getId() == 1L;
+            }
+        }))).thenReturn(List.of());
+
+        assertThrows(AccountException.class,
+                () -> paymentProcessor.makeTransfer(sourceAgreement, destinationAgreement,
+                        0, 0, BigDecimal.ONE));
+    }
+
+    @Test
+    public void makeTransferWithNoDestinationAccount() {
+        Agreement sourceAgreement = new Agreement();
+        sourceAgreement.setId(1L);
+
+        Agreement destinationAgreement = new Agreement();
+        destinationAgreement.setId(2L);
+
+        Account sourceAccount = new Account();
+        sourceAccount.setAmount(BigDecimal.TEN);
+        sourceAccount.setType(0);
+
+        when(accountService.getAccounts(argThat(new ArgumentMatcher<Agreement>() {
+            @Override
+            public boolean matches(Agreement argument) {
+                return argument != null && argument.getId() == 1L;
+            }
+        }))).thenReturn(List.of(sourceAccount));
+
+        when(accountService.getAccounts(argThat(new ArgumentMatcher<Agreement>() {
+            @Override
+            public boolean matches(Agreement argument) {
+                return argument != null && argument.getId() == 2L;
+            }
+        }))).thenReturn(List.of());
+
+        assertThrows(AccountException.class,
+                () -> paymentProcessor.makeTransfer(sourceAgreement, destinationAgreement,
+                        0, 0, BigDecimal.ONE));
     }
 
 }
